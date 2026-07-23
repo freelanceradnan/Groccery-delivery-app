ecb3e54acc4dff70a1ef2d982ea246fe82734ca7
freelanceradnan <reactorbro722@gmail.com>
2026-07-23 14:14:35 +0600

    mongodb connection added

--- server/App/Config/ConnectDB.js
+++ server/App/Config/ConnectDB.js
@@ -1,17 +1,41 @@
 import mongoose from "mongoose";
-export const ConnectDB = async () => {
-    const ConnectUrl = process.env.MONGODB_URL;
 
-    try {
-        if (!ConnectUrl) {
-            console.log('MongoDB Connection URL Missing');
-            return;
-        }
-        
-        await mongoose.connect(ConnectUrl, { autoIndex: true });
-        console.log('MongoDB connection success!');
-        
-    } catch (error) {
-        console.log('MongoDB connection failed!', error.message);
-    }
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export const ConnectDB = async () => {
+  const ConnectUrl = process.env.MONGODB_URL;
+
+  if (!ConnectUrl) {
+    console.log("MongoDB Connection URL Missing in Environment Variables!");
+    return;
+  }
+
+  if (cached.conn) {
+    return cached.conn;
+  }
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false, 
+      autoIndex: true,
+    };
+
+    cached.promise = mongoose.connect(ConnectUrl, opts).then((mongoose) => {
+      console.log("MongoDB connection success!");
+      return mongoose;
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    console.log("MongoDB connection failed!", error.message);
+    throw error;
+  }
+
+  return cached.conn;
 };
